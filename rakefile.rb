6eee7f50b40ba12324dce4d452c3e78f51e51175
macbookpr02010 <macbookpro2010@gianduia.net>
2016-05-12 20:48:50 +0200

    Gemfile.lock 3

--- rakefile.rb
+++ rakefile.rb
@@ -8,7 +8,7 @@ task :default => :spec
 
 desc "Run specs"
 RSpec::Core::RakeTask.new do |t|
-  t.pattern = "**/*_spec.rb" # don't need this, it's default.
+  t.pattern = "spec/**/*_spec.rb" # don't need this, it's default.
   # Put spec opts in a file named .rspec in root
 end
 
